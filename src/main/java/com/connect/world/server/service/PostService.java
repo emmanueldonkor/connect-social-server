10b322cab7688c9197b1dc19e4f999cc33d4dd94
Emmanuel Donkor <edonkor0000@gmail.com>
2023-09-13 17:26:01 +0200

    updated delete mapping impl

--- src/main/java/com/connect/world/server/service/PostService.java
+++ src/main/java/com/connect/world/server/service/PostService.java
@@ -4,9 +4,12 @@ package com.connect.world.server.service;
 import com.connect.world.server.model.Post;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PostService {
   Post addPost(Post post) throws Exception;
 
   List<Post> getPost();
+
+  void deletePostById(UUID id) throws Exception;
 }
